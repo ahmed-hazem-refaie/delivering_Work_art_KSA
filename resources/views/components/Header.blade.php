0ce3b7664adba174622dfc66c235db4347b2b618
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-06-24 22:27:57 +0200

    Home Responsive

---
+++ resources/views/components/Header.blade.php
@@ -0,0 +1,19 @@
+ <div class="hero hero-classic"> 
+    <div class="row">
+        <img class="hero-image-md" src="//cdn.shopify.com/s/files/1/3000/4362/files/desktop-hero-1_2048x.jpg?v=1592197390" alt="Fine art">
+        <div class="col-md-12">
+            <img class="hero-image-pc" src="//cdn.shopify.com/s/files/1/3000/4362/files/desktop-hero-1_2048x.jpg?v=1592197390" alt="Fine art">
+            <div class="hero-content">
+                <p class="hero-subtitle">INTRODUCING</p>
+                <h3 class="hero-title">Fine art</h3>
+                <div class="hero-counter">
+                    <span class="hero-counter__live">Live</span>
+                    <img class="hero-counter__eye" src="//cdn.shopify.com/s/files/1/3000/4362/t/109/assets/eye.png?v=10839783198912625093">
+                    <span id="hero-counter">408</span>
+                    shoppers
+                </div>
+                <a href="https://ecstase.com/pages/walltones?artwork=synthetic-3" class="hero-btn btn">Shop now</a>
+            </div>
+        </div>
+    </div>
+ </div>
