1d13ebf0e78f21949173452042d0a07104998af6
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-06-24 09:48:40 +0200

    Slider in Home

--- resources/views/userLayout/home.blade.php
+++ resources/views/userLayout/home.blade.php
@@ -17,12 +17,226 @@
 
 	</div>
 </div>
-<div class="container">
+<div class="container-fluid">
 	<h2 class="home-products-slider__title section-title">
 		<span>New Release</span>
 		<span class="sTitle">THIS WEEK’S RELEASE OF LIMITED ARTWORKS</span>
 	</h2>
-	<div class="owl-carousel">
+	<div class="swiper-container mt-5">
+		<div class="swiper-wrapper">
+		  <div class="swiper-slide">
+			<div class="product-grid-item">
+				<div class="product-grid-item__image">
+					<a class="product-grid-item__imagewrapper" href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">
+						<img class="front" src="//cdn.shopify.com/s/files/1/3000/4362/products/SonthisistheUniverseEditions_Product_Image_1_1024x1024.jpg?v=1592233374">
+						<img class="back" src="//cdn.shopify.com/s/files/1/3000/4362/products/Son-this-is-the-Universe-DD-1_1024x1024.jpg?v=1592233469">
+					</a>
+					<div class="product-grid-item__variants">
+						<span>Size</span>
+						<div class="product-grid-item__variants--content">
+							<a href="#" data-cart-add="32583351238759" class="soldout">L</a>
+							<a href="#" data-cart-add="32583351337063" class="soldout">M</a>
+							<a href="#" data-cart-add="32583351402599" class="">S</a>
+						</div>
+					</div>
+					<div class="product-grid-item__tags">
+						<div class="cms-special-description">
+							<span class="cms-special-label color-4">DIAMOND DUST</span>
+							<p>Walltones-studio added diamond dust to this print for a glittering effect that makes the artwork even more enticing. Additionally, keep in mind that each artwork is done by hand and therefore may slightly vary from the exemplary product images.</p>
+						</div>
+						<div class="cms-special-description">
+							<span class="cms-special-label color-7">NEW RELEASE</span>
+							<p>This artwork was added recently (but will likely be sold out soon)</p>
+						</div>
+					</div>
+				</div>
+				<div class="product-grid-item__info">
+					<div class="product-grid-item__title">
+						<h3 class="product-grid-item__name"><a href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">Son, this is the Universe</a></h3>
+						<span class="product-grid-item__price">from $199</span>
+					</div>
+					<span class="product-grid-item__qty"><span class="prints-times">edition of 50 - </span><span class="black">48 left</span></span>
+				</div>
+			</div>
+		  </div>
+		  <div class="swiper-slide">
+			<div class="product-grid-item">
+				<div class="product-grid-item__image">
+					<a class="product-grid-item__imagewrapper" href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">
+						<img class="front" src="//cdn.shopify.com/s/files/1/3000/4362/products/SonthisistheUniverseEditions_Product_Image_1_1024x1024.jpg?v=1592233374">
+						<img class="back" src="//cdn.shopify.com/s/files/1/3000/4362/products/Son-this-is-the-Universe-DD-1_1024x1024.jpg?v=1592233469">
+					</a>
+					<div class="product-grid-item__variants">
+						<span>Size</span>
+						<div class="product-grid-item__variants--content">
+							<a href="#" data-cart-add="32583351238759" class="soldout">L</a>
+							<a href="#" data-cart-add="32583351337063" class="soldout">M</a>
+							<a href="#" data-cart-add="32583351402599" class="">S</a>
+						</div>
+					</div>
+					<div class="product-grid-item__tags">
+						<div class="cms-special-description">
+							<span class="cms-special-label color-4">DIAMOND DUST</span>
+							<p>Walltones-studio added diamond dust to this print for a glittering effect that makes the artwork even more enticing. Additionally, keep in mind that each artwork is done by hand and therefore may slightly vary from the exemplary product images.</p>
+						</div>
+						<div class="cms-special-description">
+							<span class="cms-special-label color-7">NEW RELEASE</span>
+							<p>This artwork was added recently (but will likely be sold out soon)</p>
+						</div>
+					</div>
+				</div>
+				<div class="product-grid-item__info">
+					<div class="product-grid-item__title">
+						<h3 class="product-grid-item__name"><a href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">Son, this is the Universe</a></h3>
+						<span class="product-grid-item__price">from $199</span>
+					</div>
+					<span class="product-grid-item__qty"><span class="prints-times">edition of 50 - </span><span class="black">48 left</span></span>
+				</div>
+			</div>
+		  </div>
+		  <div class="swiper-slide">
+			<div class="product-grid-item">
+				<div class="product-grid-item__image">
+					<a class="product-grid-item__imagewrapper" href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">
+						<img class="front" src="//cdn.shopify.com/s/files/1/3000/4362/products/SonthisistheUniverseEditions_Product_Image_1_1024x1024.jpg?v=1592233374">
+						<img class="back" src="//cdn.shopify.com/s/files/1/3000/4362/products/Son-this-is-the-Universe-DD-1_1024x1024.jpg?v=1592233469">
+					</a>
+					<div class="product-grid-item__variants">
+						<span>Size</span>
+						<div class="product-grid-item__variants--content">
+							<a href="#" data-cart-add="32583351238759" class="soldout">L</a>
+							<a href="#" data-cart-add="32583351337063" class="soldout">M</a>
+							<a href="#" data-cart-add="32583351402599" class="">S</a>
+						</div>
+					</div>
+					<div class="product-grid-item__tags">
+						<div class="cms-special-description">
+							<span class="cms-special-label color-4">DIAMOND DUST</span>
+							<p>Walltones-studio added diamond dust to this print for a glittering effect that makes the artwork even more enticing. Additionally, keep in mind that each artwork is done by hand and therefore may slightly vary from the exemplary product images.</p>
+						</div>
+						<div class="cms-special-description">
+							<span class="cms-special-label color-7">NEW RELEASE</span>
+							<p>This artwork was added recently (but will likely be sold out soon)</p>
+						</div>
+					</div>
+				</div>
+				<div class="product-grid-item__info">
+					<div class="product-grid-item__title">
+						<h3 class="product-grid-item__name"><a href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">Son, this is the Universe</a></h3>
+						<span class="product-grid-item__price">from $199</span>
+					</div>
+					<span class="product-grid-item__qty"><span class="prints-times">edition of 50 - </span><span class="black">48 left</span></span>
+				</div>
+			</div>
+		  </div>
+		  <div class="swiper-slide">
+			<div class="product-grid-item">
+				<div class="product-grid-item__image">
+					<a class="product-grid-item__imagewrapper" href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">
+						<img class="front" src="//cdn.shopify.com/s/files/1/3000/4362/products/SonthisistheUniverseEditions_Product_Image_1_1024x1024.jpg?v=1592233374">
+						<img class="back" src="//cdn.shopify.com/s/files/1/3000/4362/products/Son-this-is-the-Universe-DD-1_1024x1024.jpg?v=1592233469">
+					</a>
+					<div class="product-grid-item__variants">
+						<span>Size</span>
+						<div class="product-grid-item__variants--content">
+							<a href="#" data-cart-add="32583351238759" class="soldout">L</a>
+							<a href="#" data-cart-add="32583351337063" class="soldout">M</a>
+							<a href="#" data-cart-add="32583351402599" class="">S</a>
+						</div>
+					</div>
+					<div class="product-grid-item__tags">
+						<div class="cms-special-description">
+							<span class="cms-special-label color-4">DIAMOND DUST</span>
+							<p>Walltones-studio added diamond dust to this print for a glittering effect that makes the artwork even more enticing. Additionally, keep in mind that each artwork is done by hand and therefore may slightly vary from the exemplary product images.</p>
+						</div>
+						<div class="cms-special-description">
+							<span class="cms-special-label color-7">NEW RELEASE</span>
+							<p>This artwork was added recently (but will likely be sold out soon)</p>
+						</div>
+					</div>
+				</div>
+				<div class="product-grid-item__info">
+					<div class="product-grid-item__title">
+						<h3 class="product-grid-item__name"><a href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">Son, this is the Universe</a></h3>
+						<span class="product-grid-item__price">from $199</span>
+					</div>
+					<span class="product-grid-item__qty"><span class="prints-times">edition of 50 - </span><span class="black">48 left</span></span>
+				</div>
+			</div>
+		  </div>
+		  <div class="swiper-slide"><div class="product-grid-item">
+			<div class="product-grid-item__image">
+				<a class="product-grid-item__imagewrapper" href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">
+					<img class="front" src="//cdn.shopify.com/s/files/1/3000/4362/products/SonthisistheUniverseEditions_Product_Image_1_1024x1024.jpg?v=1592233374">
+					<img class="back" src="//cdn.shopify.com/s/files/1/3000/4362/products/Son-this-is-the-Universe-DD-1_1024x1024.jpg?v=1592233469">
+				</a>
+				<div class="product-grid-item__variants">
+					<span>Size</span>
+					<div class="product-grid-item__variants--content">
+						<a href="#" data-cart-add="32583351238759" class="soldout">L</a>
+						<a href="#" data-cart-add="32583351337063" class="soldout">M</a>
+						<a href="#" data-cart-add="32583351402599" class="">S</a>
+					</div>
+				</div>
+				<div class="product-grid-item__tags">
+					<div class="cms-special-description">
+						<span class="cms-special-label color-4">DIAMOND DUST</span>
+						<p>Walltones-studio added diamond dust to this print for a glittering effect that makes the artwork even more enticing. Additionally, keep in mind that each artwork is done by hand and therefore may slightly vary from the exemplary product images.</p>
+					</div>
+					<div class="cms-special-description">
+						<span class="cms-special-label color-7">NEW RELEASE</span>
+						<p>This artwork was added recently (but will likely be sold out soon)</p>
+					</div>
+				</div>
+			</div>
+			<div class="product-grid-item__info">
+				<div class="product-grid-item__title">
+					<h3 class="product-grid-item__name"><a href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">Son, this is the Universe</a></h3>
+					<span class="product-grid-item__price">from $199</span>
+				</div>
+				<span class="product-grid-item__qty"><span class="prints-times">edition of 50 - </span><span class="black">48 left</span></span>
+			</div>
+		</div></div>
+		  <div class="swiper-slide">
+			<div class="product-grid-item">
+				<div class="product-grid-item__image">
+					<a class="product-grid-item__imagewrapper" href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">
+						<img class="front" src="//cdn.shopify.com/s/files/1/3000/4362/products/SonthisistheUniverseEditions_Product_Image_1_1024x1024.jpg?v=1592233374">
+						<img class="back" src="//cdn.shopify.com/s/files/1/3000/4362/products/Son-this-is-the-Universe-DD-1_1024x1024.jpg?v=1592233469">
+					</a>
+					<div class="product-grid-item__variants">
+						<span>Size</span>
+						<div class="product-grid-item__variants--content">
+							<a href="#" data-cart-add="32583351238759" class="soldout">L</a>
+							<a href="#" data-cart-add="32583351337063" class="soldout">M</a>
+							<a href="#" data-cart-add="32583351402599" class="">S</a>
+						</div>
+					</div>
+					<div class="product-grid-item__tags">
+						<div class="cms-special-description">
+							<span class="cms-special-label color-4">DIAMOND DUST</span>
+							<p>Walltones-studio added diamond dust to this print for a glittering effect that makes the artwork even more enticing. Additionally, keep in mind that each artwork is done by hand and therefore may slightly vary from the exemplary product images.</p>
+						</div>
+						<div class="cms-special-description">
+							<span class="cms-special-label color-7">NEW RELEASE</span>
+							<p>This artwork was added recently (but will likely be sold out soon)</p>
+						</div>
+					</div>
+				</div>
+				<div class="product-grid-item__info">
+					<div class="product-grid-item__title">
+						<h3 class="product-grid-item__name"><a href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">Son, this is the Universe</a></h3>
+						<span class="product-grid-item__price">from $199</span>
+					</div>
+					<span class="product-grid-item__qty"><span class="prints-times">edition of 50 - </span><span class="black">48 left</span></span>
+				</div>
+			</div>
+		  </div>
+		</div>
+		<!-- Add Pagination -->
+		<div class="swiper-pagination"></div>
+	  </div>
+	{{-- <div class="owl-carousel">
 		<div class="product-grid-item">
 			<div class="product-grid-item__image">
 				<a class="product-grid-item__imagewrapper" href="/products/son-this-is-the-universe" data-product-handle="son-this-is-the-universe" data-product-quantity="48">
@@ -56,7 +270,7 @@
 				<span class="product-grid-item__qty"><span class="prints-times">edition of 50 - </span><span class="black">48 left</span></span>
 			</div>
 		</div>
-	</div>
+	</div> --}}
 </div>
 
 
@@ -73,5 +287,14 @@
 	$(document).ready(function() {
 		$(".owl-carousel").owlCarousel();
 	});
+	var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      freeMode: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
 </script>
 @endsection
