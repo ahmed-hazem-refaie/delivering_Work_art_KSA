d6b7f0a6142a964be09ed425001fd3291f018350
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-06-25 02:04:54 +0200

    Home Background Video

--- resources/views/components/Video.blade.php
+++ resources/views/components/Video.blade.php
@@ -1,7 +1,7 @@
 <header class="mb-4">
     <div class="overlay"></div>
     <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-      <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
+      <source src="https://cdn.shopify.com/s/files/1/3000/4362/files/16-9Desktop.mp4?v=1592998652" type="video/mp4">
     </video>
     <div class="text">
         <h1>Aboute ECSTASE</h1>
